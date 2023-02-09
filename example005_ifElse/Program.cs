850b352053fd2493903124e654eadfe0a224a99b
Alexey M <baoctos@gmail.com>
2023-02-09 22:06:19 +0600

    add example005 ifElse greetings Glasha

---
+++ example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите ваше имя: ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "глаша")
+{
+    System.Console.WriteLine("Ого! Какие люди!");
+}
+else
+{
+    System.Console.WriteLine("Привет, рядовой пользователь," + userName + ":(");
+}
